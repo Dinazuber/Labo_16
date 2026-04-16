ff29b88c99644e272730f18567043bcbb60bb68b
Dina <119934536+Dinazuber@users.noreply.github.com>
2026-04-16 15:31:05 +0200

    [ADD] Method to do the SelectSort

--- src/work/SelectionSort.scala
+++ src/work/SelectionSort.scala
@@ -2,8 +2,17 @@ package work
 
 class SelectionSort extends Sort {
   override def sort(a: Array[Int]): Array[Int] = {
-    println("salut")
-
+    for(i: Int <- 0 until a.length){
+      var temp : Int = a(i)
+      var minId: Int = i
+      for(j: Int <- i until a.length){
+        if(a(minId) > a(j)){
+          minId = j
+        }
+      }
+      a(i) = a(minId)
+      a(minId) = temp
+    }
     return a
   }
 }
