15c359f8e3dd0b14ec864adeac2243b368c2f4f4
Dina <119934536+Dinazuber@users.noreply.github.com>
2026-04-28 20:11:00 +0200

    [Update] The display of the arrays

--- src/work/SortApplication.scala
+++ src/work/SortApplication.scala
@@ -3,13 +3,15 @@ object SortApplication {
     println(a.mkString(","))
   }
   def main(args : Array[String]) : Unit = {
-    val size : Int = 100000
+    val size : Int = 70000
     var almostSortedArray :  Array[Int] = AlmostSortedArrayFactory.create(size)
     var invertedSortedArray :  Array[Int] = InvertedSortedArrayFactory.create(size)
     var randomArray :  Array[Int] = RandomArrayFactory.create(size)
     var shuffleArrayFactory :  Array[Int] = ShuffleArrayFactory.create(size)
-    //print(almostSortedArray.mkString(",") + '\n' + invertedSortedArray.mkString(",") + '\n' +
-    //  randomArray.mkString(",") + '\n' + shuffleArrayFactory.mkString(","))
+    display(almostSortedArray)
+    display(invertedSortedArray)
+    display(randomArray)
+    display(shuffleArrayFactory)
     var arrays : Array[Array[Int]] = Array(almostSortedArray, invertedSortedArray, randomArray, shuffleArrayFactory)
     var temp = arrays.map(_.clone()) //Deep clone of the table
 
